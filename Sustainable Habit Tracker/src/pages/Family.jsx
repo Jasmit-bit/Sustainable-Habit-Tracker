e82bc41148887f5966849547731ecd984931ef0c
Jasmit-bit <jxs1918@student.bham.ac.uk>
2026-03-12 18:01:33 +0000

    Started working on the family section

--- Sustainable Habit Tracker/src/pages/Family.jsx
+++ Sustainable Habit Tracker/src/pages/Family.jsx
@@ -1,7 +1,23 @@
+import { useState, useEffect, use} from 'react';
+import {supabase} from '../supabaseClient';
+import './Family.css';
+
 export default function Family() {
-  return (
-    <div style={{ padding: '20px', textAlign: 'center' }}>
-      <h1>Family & Household</h1>
-    </div>
-  )
-}
+  const[loading,setLoading] = useState(true);
+  const [hasHousehld, setHasHousehold] = useState(false);
+  const [householdName, setHouseholdName] = useState('');
+  const [inviteCode, setInviteCode] = useState(''); 
+
+  useEffect(() => {
+    checkHouseExists();
+  }, []);
+
+  // because every user starts off with no household I need to check if they have a household or not, if they do then I set that otherwise I show I different screen 
+  // which tells them to create or join a household
+
+  async function checkHouseExists() {
+    const user = supabase.auth.user();
+    
+
+
+
