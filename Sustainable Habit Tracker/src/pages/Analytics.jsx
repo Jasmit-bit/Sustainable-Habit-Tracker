f913744f1d7175cffa7787ea68f877fc25e6d395
Gabriela Woodroffe-Gomez <gew450@student.bham.ac.uk>
2026-03-16 13:19:39 +0000

    error handling

--- Sustainable Habit Tracker/src/pages/Analytics.jsx
+++ Sustainable Habit Tracker/src/pages/Analytics.jsx
@@ -5,35 +5,53 @@ export default function Analytics() {
   const [habitLogs, setHabitLogs] = useState([]);
   const [loading, setLoading] = useState(true);
 
+  const [error, setError] = useState(false);
+  const [errorMessage, setErrorMessage] = useState('');
+
   useEffect(() => {
     fetchLogs()
-}, [])
+  }, [])
 
   //fetch the habit log data from supabase 
   async function fetchLogs() {
-    const {data: {user}} = await supabase.auth.getUser();
+    try {
+      const {data: {user}} = await supabase.auth.getUser();
 
-    const {data} = await supabase
-      .from('habit_logs')
-      .select('*')
-      .eq('user_id', user.id);
+      const {data: logsData} = await supabase
+        .from('habit_logs')
+        .select('*')
+        .eq('user_id', user.id);
+        
+      setHabitLogs(logsData);
 
-    setHabitLogs(data);
-    setLoading(false);
+    } catch (error) {
+      setError(true);
+      setErrorMessage(error.message);
+    } finally {
+      setLoading(false);
+    }
   }
 
   //pass the logs into the function to calculate the running total of all co2 saved in order to display "lifetime" stats
   function calculateCO2Saved(logs) {
-    return logs.reduce((total, current) => {
-      return total + current.total_co2_saved;
-    }, 0)
+    try {
+      return logs.reduce((total, current) => {
+        return total + current.total_co2_saved;
+      }, 0)
+    } catch (error) {
+      return 0;
+    } 
   }
 
   //do the same for total plastic saved
   function calculatePlasticSaved(logs) {
-    return logs.reduce((total, current) => {
-      return total + current.total_plastic_saved;
-    }, 0)
+    try {
+      return logs.reduce((total, current) => {
+        return total + current.total_plastic_saved;
+      }, 0)
+    } catch (error) {
+      return 0;
+    }
   }
 
   //check if in loading screen - styled to match code on other pages for consistency
@@ -44,6 +62,9 @@ export default function Analytics() {
 
   return (
     <div style={{ padding: '20px', textAlign: 'center' }}>
+      {/* return for error message */}
+      {error && <div>{errorMessage}</div>}
+
       <h1> Analytics: </h1>
       <p>Total CO2 Saved: {calculateCO2Saved(habitLogs)}</p>
       <p>Total Plastic Saved: {calculatePlasticSaved(habitLogs)}</p>
