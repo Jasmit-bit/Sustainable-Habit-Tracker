606b6beadeb13003e92d596d44ed6205f651c69c
Gabriela Woodroffe-Gomez <gew450@student.bham.ac.uk>
2026-03-16 11:44:51 +0000

    added function to fetch data from supabase

--- Sustainable Habit Tracker/src/pages/Analytics.jsx
+++ Sustainable Habit Tracker/src/pages/Analytics.jsx
@@ -1,4 +1,29 @@
+import {useState, useEffect} from 'react';
+import {supabase} from '../supabaseClient';
+
 export default function Analytics() {
+  const [habitLogs, setHabitLogs] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetchLogs()
+}, [])
+
+  async function fetchLogs() {
+    const {data: {user}} = await supabase.auth.getUser();
+
+    const {data} = await supabase
+      .from('habit_logs')
+      .select('*')
+      .eq('user_id', user.id);
+
+    setHabitLogs(data);
+  }
+
+
+
+
+
   return (
     <div style={{ padding: '20px', textAlign: 'center' }}>
       <h1> Analytics</h1>
